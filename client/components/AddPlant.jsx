282e9953a36f08f4ceacfb2805a957b8fb3e103f
Kang-Gao <64079938+gawk2333@users.noreply.github.com>
2021-09-16 02:50:35 +1200

    changed select

--- client/components/AddPlant.jsx
+++ client/components/AddPlant.jsx
@@ -6,21 +6,26 @@ import ReactDropdown from 'react-dropdown'
 
 function AddPlant(props)
 {
+  const [speCies,setSpecies] = useState("")
   var species = props.species
-  var speciesArr=[]
-  const defaultOption = species[0]
-  species.map(s=>speciesArr.push(s.scientific))
-  useEffect(()=>{
-
-  },[])
-
+  // var speciesArr=[]
+  // const defaultOption = species[0]
+  // species.map(s=>speciesArr.push(s.scientific))
+  const speciesChangeHandler = (i)=>{
+    console.log(i)
+  }
   return (
     <>
     <div>Name:<input id='name'></input></div>
     <div>Note:<input id='note'></input></div>
-    <div>Species:<ReactDropdown options={speciesArr}></ReactDropdown></div>
- 
-    
+    <div>Species:<select>
+    {/* <div>Species:<ReactDropdown options={speciesArr}></ReactDropdown></div> */
+      species.map((s)=>{
+        return <option key={s.id} onChange={(key)=>speciesChangeHandler(key)}>{s.scientific}({s.common})</option>
+      })
+    }
+     </select>
+    </div>
     </>
   )
 }
