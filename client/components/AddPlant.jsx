c1c8de586e7314c9fbe1811adad04e2c22ff5865
Jo Tregidga <84287388+jo-tree@users.noreply.github.com>
2021-09-17 14:17:47 +1200

    nav x2

--- client/components/AddPlant.jsx
+++ client/components/AddPlant.jsx
@@ -90,7 +90,7 @@ function AddPlant(props){
         </select>
         <br/>
       <button>Add to my plants</button>
-      <Link to={`/species`}><button>Add a new species</button></Link>
+      <Link to={`/species/add`}><button>Add a new species</button></Link>
     </form>
     </div>
   )
